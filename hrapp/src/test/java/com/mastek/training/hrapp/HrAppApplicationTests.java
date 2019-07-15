1571c769dcd2b3cbf04ba9455ed9e2966f939cc2
Gregory61171 <Gregory61171@GB-LD1LPW10G026.mastek.com>
2019-07-15 14:59:46 +0100

    Employee Entity with related methods and test cases

--- hrapp/src/test/java/com/mastek/training/hrapp/HrAppApplicationTests.java
+++ hrapp/src/test/java/com/mastek/training/hrapp/HrAppApplicationTests.java
@@ -1,5 +1,12 @@
 package com.mastek.training.hrapp;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +35,32 @@ public class HrAppApplicationTests {
 	
 	@Test
 	public void addEmployeeUsingService() {
-		empService.registerEmployee(emp);
+		emp.setEmpno(6); //setting empno to 0, the default, will add a new employee, setting empno to a current number will update it.
+		emp.setName("new guy 6");
+		emp.setSalary(5434);
+		emp = empService.registerOrUpdateEmployee(emp);
+		assertNotNull(emp);
 	}
+	
+	@Test
+	public void findByEmpnoUsingService() {
+		int empno = 1;
+		assertNotNull(empService.findByEmpno(empno));
+	}	
+	@Test
+	public void deleteByEmpnoUsingService() {
+		int empno = 5;
+		empService.deleteByEmpno(empno);
+		assertNull(empService.findByEmpno(empno));
+	}
+	
+	@Test
+	public void checkFetchBySalary() {
+		List<Employee> emps = empService.fetchEmployeesBySalaryRange(0, 9000);
+		for (Employee employee : emps) {
+			System.out.println(employee);
+			}
+		assertEquals(emps.size(),7);}	
 	
 	@Test
 	public void simpleTest() {
